12a4dc083fdf2fe85fbe9fc65f219481bf3bdd5c
Erik Olofsson <erik@olofsson.info>
2018-02-17 10:19:49 +0100

    [Malterlib/Function] Add fg_MergeFunctors helper

---
+++ Source/Malterlib_Function_MergeFunctors.h
@@ -0,0 +1,18 @@
+// Copyright © 2018 Nonna Holding AB
+// Distributed under the MIT license, see license text in LICENSE.Malterlib
+
+#include "Private/Malterlib_Function_MergeFunctors.h"
+
+namespace NMib::NFunction
+{
+	template <class ...tfp_FToMerge>
+	auto fg_MergeFunctors(tfp_FToMerge &&...p_fToMerge)
+	{
+		using CMergedFunctors = NPrivate::TCMergedFunctors<typename NTraits::TCDecay<tfp_FToMerge>::CType...>;
+		return CMergedFunctors(fg_Forward<tfp_FToMerge>(p_fToMerge)...);
+	};
+}
+
+#ifndef DMibPNoShortCuts
+	using namespace NMib::NFunction;	
+#endif
---
+++ Source/Private/Malterlib_Function_MergeFunctors.h
@@ -0,0 +1,38 @@
+// Copyright © 2018 Nonna Holding AB 
+// Distributed under the MIT license, see license text in LICENSE.Malterlib
+
+#pragma once
+
+namespace NMib::NFunction::NPrivate
+{
+	template <typename ...tp_CFunctors>
+	struct TCMergedFunctors;
+
+	template <typename t_CFirstFunctor, typename ...tp_CFunctors>
+	struct TCMergedFunctors<t_CFirstFunctor, tp_CFunctors...>
+		: public TCMergedFunctors<tp_CFunctors...>
+		, public t_CFirstFunctor
+	{
+
+		using t_CFirstFunctor::operator ();
+		using TCMergedFunctors<tp_CFunctors...>::operator ();
+
+		template <typename tf_CFirstFunctor, typename ...tfp_CFunctors>
+		TCMergedFunctors(tf_CFirstFunctor &&_FirstFunctor, tfp_CFunctors ...p_Functors)
+			: t_CFirstFunctor(fg_Forward<tf_CFirstFunctor>(_FirstFunctor))
+			, TCMergedFunctors<tp_CFunctors...>(fg_Forward<tfp_CFunctors>(p_Functors)...)
+		{
+		}
+	};
+
+	template<typename t_CFirstFunctor>
+	struct TCMergedFunctors<t_CFirstFunctor> : public t_CFirstFunctor
+	{
+		using t_CFirstFunctor::operator ();
+
+		TCMergedFunctors(t_CFirstFunctor l1)
+			: t_CFirstFunctor(l1)
+		{
+		}
+	};
+}
