954177ae421df1072430ce716acac4da3b52f0cd
Erik Olofsson <erik.olofsson@favro.com>
2020-11-17 00:16:29 +0100

    [Malterlib/Function] Remove MSVC workaround

--- Source/Private/Malterlib_Function_Function_NullFunction.h
+++ Source/Private/Malterlib_Function_Function_NullFunction.h
@@ -15,57 +15,16 @@ namespace NMib::NFunction::NPrivate
 
 	struct CReturnReference
 	{
-#if defined(DCompiler_MSVC_Workaround)
-	public:
-#else
 	private:
-#endif
 		friend class CNullFunctionImpl;
 		template <typename t_CBase, typename t_FFunc, int t_Qualifiers>
 		friend struct TCCallImpl;
 
-#if defined(DCompiler_MSVC_Workaround)
-#if 1
-		template
-			<
-				typename t_CReturn
-				, typename TCEnableIf
-				<
-					NTraits::TCIsConstructorCallableWith<t_CReturn, void (t_CReturn&&)>::mc_Value
-					&& !NTraits::TCIsConstructorCallableWith<t_CReturn, void (t_CReturn&)>::mc_Value
-				>::CType * = nullptr
-			>
-		__declspec(noreturn) operator t_CReturn &&() const
-		{
-			throw 1; // Should never get here
-		}
-
-		template
-			<
-				typename t_CReturn
-				, typename TCEnableIf
-				<
-					NTraits::TCIsConstructorCallableWith<t_CReturn, void (t_CReturn&)>::mc_Value
-				>::CType * = nullptr
-			>
-		__declspec(noreturn) operator t_CReturn &() const
-		{
-			throw 1; // Should never get here
-		}
-#else
-		template <typename t_CReturn>
-		__declspec(noreturn) operator t_CReturn && () const
-		{
-			fg_NoReturn();
-		}
-#endif
-#else
 		template <typename t_CReturn>
 		[[noreturn]] operator t_CReturn && () const
 		{
 			fg_NoReturn();
 		}
-#endif
 	};
 
 	class CNullFunctionImpl
